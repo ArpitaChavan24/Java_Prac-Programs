e338a30fe4de43f8a1e44e586690460548f4ca8b
ArpitaChavan24 <aschavan24106@gmail.com>
2025-01-17 23:56:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //Implicit Casting
+        int p = 100;
+        double q = p; //int to double
+        System.out.println(q);
+
+        //Explicit Casting
+        double x = 99.99;
+        int y = (int)x; // double to int
+        System.out.println(y);
+
+    }
+}
